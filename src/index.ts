ec0360a96f15fac386f8c5f588e3cdfff56e7b00
Pravendra0421 <pravendrajain421@gmail.com>
2025-08-20 14:53:17 +0530

    add the js in userRoute

--- src/index.ts
+++ src/index.ts
@@ -4,7 +4,7 @@ import express, { Request, Response } from "express";
 import ConnectDb from "./config/database.js";
 import "./config/firebaseAdmin.js";
 import prisma from "./lib/prisma";
-import authRoutes from "./routes/userRoute";
+import authRoutes from "./routes/userRoute.js";
 import cors from "cors";
 const app = express();
 
